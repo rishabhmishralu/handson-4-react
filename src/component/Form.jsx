be16d55d416b0d826274b50c260c351518f9c672
= <=>
2023-01-25 23:57:49 +0530

    add

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -6,43 +6,27 @@ import Student from "./Student";
 
 
 const Form = () => {
-  const [name, setname] = useState();
-  const [age, setage] = useState();
-  const [course, setcourse] = useState();
-  const [batch, setbatch] = useState();
-  const [allEntry, setallEntry] = useState([]);
+
 
   const Navigate = useNavigate();
   const go = () => {
-    Navigate(-1);
-  };
-
-  const submitform = (e) => {
-    e.preventDefault();
-    const newEntry = {
-      name: name,
-      age: age,
-      course: course,
-      batch: batch,
-    };
-    setallEntry([...allEntry, newEntry]);
-    console.log(allEntry);
-  };
+   
+        Navigate(-1);
+      };
+    
 
   return (
     <div>
       
         <div className="form">
         <div className="innerform">
-          <form action="" onSubmit={submitform}>
+          <form action="" >
             <label className="label" htmlFor="name">
               Name :{" "}
             </label>
             <input
               type="text"
-              name="name"
-              value={name}
-              onChange={(e) => setname(e.target.value)}
+          
               className="text-input"
             />
             <br></br><br />
@@ -51,9 +35,7 @@ const Form = () => {
             </label>
             <input
               type="text"
-              name="age"
-              value={age}
-              onChange={(e) => setage(e.target.value)}
+            
               className="text-input"
             />
             <br></br><br />
@@ -62,9 +44,7 @@ const Form = () => {
             </label>
             <input
               type="text"
-              name="course"
-              value={course}
-              onChange={(e) => setcourse(e.target.value)}
+            
               className="text-input"
             />
             <br></br><br />
@@ -73,9 +53,7 @@ const Form = () => {
             </label>
             <input
               type="text"
-              name="batch"
-              value={batch}
-              onChange={(e) => setbatch(e.target.value)}
+           
               className="text-input"
             />
             <br></br>
--- src/component/Update.jsx
+++ src/component/Update.jsx
@@ -2,28 +2,12 @@ import React from 'react'
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 const Update = () => {
-    const [name, setname] = useState();
-    const [age, setage] = useState();
-    const [course, setcourse] = useState();
-    const [batch, setbatch] = useState();
-    const [allEntry, setallEntry] = useState([]);
-  
+
     const Navigate = useNavigate();
     const go = () => {
       Navigate(-1);
     };
   
-    const submitform = (e) => {
-      e.preventDefault();
-      const newEntry = {
-        name: name,
-        age: age,
-        course: course,
-        batch: batch,
-      };
-      setallEntry([...allEntry, newEntry]);
-      console.log(allEntry);
-    };
   
   return (
     <div>
@@ -31,16 +15,14 @@ const Update = () => {
         
         <div className="form">
         <div className="innerform">
-        {/* <div className="innerform"> */}
-          <form action="" onSubmit={submitform}>
+       
+          <form action="" >
             <label className="label" htmlFor="name">
               Name :{" "}
             </label>
             <input
               type="text"
-              name="name"
-              value={name}
-              onChange={(e) => setname(e.target.value)}
+          
               className="text-input"
             />
             <br></br><br />
@@ -49,9 +31,7 @@ const Update = () => {
             </label>
             <input
               type="text"
-              name="age"
-              value={age}
-              onChange={(e) => setage(e.target.value)}
+            
               className="text-input"
             />
             <br></br><br />
@@ -60,9 +40,7 @@ const Update = () => {
             </label>
             <input
               type="text"
-              name="course"
-              value={course}
-              onChange={(e) => setcourse(e.target.value)}
+          
               className="text-input"
             />
             <br></br><br />
@@ -71,9 +49,7 @@ const Update = () => {
             </label>
             <input
               type="text"
-              name="batch"
-              value={batch}
-              onChange={(e) => setbatch(e.target.value)}
+           
               className="text-input"
             />
             <br></br>
